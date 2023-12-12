61c94dc2331be8cc29b67cdb3000df91bea77b5c
Chandan Thota <chandanthota75@gmail.com>
2023-12-12 15:53:48 +0530

    Day 20 solution.py

---
+++ Day20/solution.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+class Solution:
+    def maximumSumSubarray (self, K, Arr, N):
+        # code here
+        max_sum = 0
+        for i in range(0, K):
+            max_sum += Arr[i]
+        
+        cur = max_sum
+        for i in range(K, N):
+            cur += Arr[i] - Arr [i - K]
+            max_sum = max(max_sum, cur)
+        
+        return max_sum
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        
+        N,K = input().split()
+        N = int(N)
+        K = int(K)
+        Arr = list( map(int,input().strip().split(" ")))
+
+        ob = Solution()
+        print(ob.maximumSumSubarray(K,Arr,N))
+# } Driver Code Ends
